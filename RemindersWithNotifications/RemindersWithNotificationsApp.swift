e165a993b2573a54ff0a66beb85a0e266f6cb235
Russell Gordon <rgordon@lcs.on.ca>
2025-05-17 06:22:36 -0400

    Initial Commit

---
+++ RemindersWithNotifications/RemindersWithNotificationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersWithNotificationsApp.swift
+//  RemindersWithNotifications
+//
+//  Created by Russell Gordon on 2025-05-17.
+//
+
+import SwiftUI
+
+@main
+struct RemindersWithNotificationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
